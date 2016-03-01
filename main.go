0a2e62c00a3abb916c304a4b8dcb1e7b313546b0
Jamie Clarkson <jamiec7919@gmail.com>
2016-03-01 13:32:29 +0000

    First commit

--- main.go
+++ main.go
@@ -2,10 +2,48 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+/*
+	The vermeer command
+*/
 package main
 
-// The vermeer command
+import (
+	"flag"
+	_ "github.com/jamiec7919/vermeer/internal/camera"
+	"github.com/jamiec7919/vermeer/internal/core"
+
+	"log"
+	"os"
+)
 
 func main() {
+	flag.Parse()
 
+	filename := "test.vnf"
+
+	if fn := flag.Arg(0); fn != "" {
+		filename = fn
+	}
+
+	rc := core.NewRenderContext()
+
+	if err := rc.LoadNodeFile(filename); err != nil {
+		log.Printf("Error: LoadNodeFile: %v", err)
+		os.Exit(1)
+	}
+
+	if err := rc.PreRender(); err != nil {
+		log.Printf("Error: PreRender: %v", err)
+		os.Exit(1)
+	}
+
+	if err := rc.Render(); err != nil {
+		log.Printf("Error: Render: %v", err)
+		os.Exit(1)
+	}
+
+	if err := rc.PostRender(); err != nil {
+		log.Printf("Error: PostRender: %v", err)
+		os.Exit(1)
+	}
 }
