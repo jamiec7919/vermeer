f23e07db56c5b2ff4191378b9295a0e2edb21fbe
Jamie Clarkson <jamiec7919@gmail.com>
2016-04-24 21:20:54 +0100

    Added floor.go, Floor and Ceil float32 functions. Requires SSE4.

---
+++ math/floor.go
@@ -0,0 +1,11 @@
+// Copyright 2016 The Vermeer Light Tools Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package math
+
+// Floor returns the greatest integer value less than or equal to x.
+func Floor(x float32) float32
+
+// Ceil returns the least integer value greater than or equal to x.
+func Ceil(x float32) float32
---
+++ math/floor_amd64.s
@@ -0,0 +1,22 @@
+// Copyright 2016 The Vermeer Light Tools Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#include "textflag.h"
+
+#define Big		0x4330000000000000 // 2**52
+
+// Unlike the Go package we require SSE4.
+
+// func Floor(x float32) float32
+TEXT ·Floor(SB),NOSPLIT,$0
+	ROUNDSS $1, x+0(FP), X0
+	MOVD X0, ret+4(FP)
+	RET
+
+// func Ceil(x float64) float64
+TEXT ·Ceil(SB),NOSPLIT,$0
+	ROUNDSS $2, x+0(FP), X0
+	MOVD X0, ret+4(FP)
+	RET
+
